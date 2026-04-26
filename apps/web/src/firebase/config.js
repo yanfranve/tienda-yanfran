30ea5b966ac4cb2448651cf04a46dae3938f5f2a
Franklin Yanez <yanfranve@gmail.com>
2026-04-26 10:39:07 -0400

    actua

---
+++ apps/web/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtJcOeRTq0BbzHBtyLha0Tn_KPO0q2s9M",
+  authDomain: "credicontrol-ai.firebaseapp.com",
+  projectId: "credicontrol-ai",
+  storageBucket: "credicontrol-ai.firebasestorage.app",
+  messagingSenderId: "220256037086",
+  appId: "1:220256037086:web:c60f4176d8cdaf49773a4f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
